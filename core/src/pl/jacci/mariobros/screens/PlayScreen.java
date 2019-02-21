db8e02f5969bd38d868569aa50669d28849c6fb3
Jacci19 <jacek205@wp.pl>
2019-02-21 12:30:25 +0100

    03_Lesson_03-04; viewports, hud

--- core/src/pl/jacci/mariobros/screens/PlayScreen.java
+++ core/src/pl/jacci/mariobros/screens/PlayScreen.java
@@ -3,18 +3,36 @@ package pl.jacci.mariobros.screens;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
 
 import pl.jacci.mariobros.MarioBros;
+import pl.jacci.mariobros.scenes.Hud;
 
 public class PlayScreen implements Screen {
 
     private MarioBros game;
-    Texture texture;
+    private OrthographicCamera gameCam;
+    private Viewport gamePort;
+    private Hud hud;
 
     public PlayScreen(MarioBros game) {
+
         this.game = game;
-        texture = new Texture("badlogic.jpg");
+        //create cam used to follow mario through cam world
+        gameCam = new OrthographicCamera();
+
+        //create a FitViewport to maintain virtual aspect ratio despite screen size
+
+        //gamePort = new ScreenViewport(gameCam);                                           //można porównać wygląd apki na trzech różnych viewportach
+        //gamePort = new StretchViewport(800, 480, gameCam);
+        gamePort = new FitViewport(MarioBros.V_WIDTH, MarioBros.V_HEIGHT, gameCam);
+
+
+        //create our game HUD for scores/timers/level info
+        hud = new Hud(game.batch);
+
     }
 
     @Override
@@ -24,16 +42,15 @@ public class PlayScreen implements Screen {
 
     @Override
     public void render(float delta) {
-        Gdx.gl.glClearColor(1,0,0,1);
+        Gdx.gl.glClearColor(0,0,0,1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-        game.batch.begin();
-        game.batch.draw(texture, 0, 0);
-        game.batch.end();
+        game.batch.setProjectionMatrix(hud.stage.getCamera().combined);
+        hud.stage.draw();
     }
 
     @Override
     public void resize(int width, int height) {
-
+        gamePort.update(width, height);
     }
 
     @Override
