7391aad5d6fb6b94b7a05297e6e4d1f33b52a065
Jacci19 <jacek205@wp.pl>
2019-03-01 13:12:04 +0100

    13_Lesson_14; Adding points after break the coinbrick. Replacing image of coinbrick after break

--- core/src/pl/jacci/mariobros/sprites/Coin.java
+++ core/src/pl/jacci/mariobros/sprites/Coin.java
@@ -1,15 +1,22 @@
 package pl.jacci.mariobros.sprites;
 
 import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TiledMapTileSet;
 import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.physics.box2d.World;
 
 import pl.jacci.mariobros.MarioBros;
+import pl.jacci.mariobros.scenes.Hud;
 
 
 public class Coin extends InteractiveTileObject{
+
+    private static TiledMapTileSet tileSet;
+    private final int BLANK_COIN = 28;                                          //taki numer ma ta grafika w Tiled (27+1 bo liczone od zera w tabeli)
+
     public Coin(World world, TiledMap map, Rectangle bounds){
         super(world, map, bounds);
+        tileSet = map.getTileSets().getTileSet("NES - Super Mario Bros - Tileset");         // nazwa pliku z całym tileSet
         fixture.setUserData(this);
         setCategoryFilter(MarioBros.COIN_BIT);
     }
@@ -17,5 +24,7 @@ public class Coin extends InteractiveTileObject{
     @Override
     public void onHeadHit() {
         System.out.println("coin collision");
+        getCell().setTile(tileSet.getTile(BLANK_COIN));
+        Hud.addScore(100);
     }
 }
